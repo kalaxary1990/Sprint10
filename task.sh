785263e16c201ea4337d92ca4ea91d295c30dfc3
Kalaxary <lkalaxaryl@gmail.com>
2025-08-17 08:51:36 +0300

    firstiteration

--- task.sh
+++ task.sh
@@ -32,7 +32,7 @@ find . -name "*.txt" > dir1/summary.txt
 cat dir2/list.txt >> dir1/summary.txt
 
 # определяем переменную окружения NAME со значением "Всем студентам"
-export NAME="Всем студентам"
+NAME="Всем студентам"
 
 # запускаем task/dir2/hello.sh с переменной окружения NAME в качестве аргумента
 # вывод скрипта должен дописаться в файл task/dir1/summary.txt
